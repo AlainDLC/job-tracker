952ec07c6da9cedb8f05fd19cda7deec23365927
AlainDLC <jd_alain@hotmail.com>
2024-11-30 15:23:26 +0100

    update content

--- job-tracker/app/components/ProcessTracker.js
+++ job-tracker/app/components/ProcessTracker.js
@@ -65,8 +65,7 @@ const ProcessTracker = ({ stages, jobs, setJobs }) => {
                   dummy text of the printing and typesetting industry. Lorem
                   Ipsum has been the industry&#39;s standard dummy text ever
                   since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book. It has survived
-                  not only five centuries,
+                  and scrambled it to make a type specimen book.,
                 </h1>
               </div>
             )}
